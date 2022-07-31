b8b1cb63b195d9e5ee6dfc7e453b6b60a3a79dc9
voldegold <kva1107@gmail.com>
2022-07-31 21:10:03 +0600

    ok 3

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        double pre_result = toBeDivided;
+        double result = pre_result / 5;
+        System.out.println(result);
     }
 }
--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+
+        int first = 1;
+        int second = 10;
+        int third = 100;
+
+        System.out.print(first);
+        System.out.print(second);
+        System.out.println(third);
+
     }
 }
